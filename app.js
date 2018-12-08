10f674de6c464bbf0d3abd7500d807415a6442de
marklajs <jjm21@hotmail.co.uk>
2018-12-08 10:41:52 +0000

    Added title tags, compressed css files, added robots.txt, added itemscope to the main page

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const flash = require('connect-flash');
 const validator = require('express-validator');
 const MongoStore = require('connect-mongo')(session);
 const helmet = require('helmet')
+const compression = require('compression');
 var app = express();
 
 mongoose.connect('mongodb://localhost:27017/sansaarDb' , { useNewUrlParser: true });
@@ -28,6 +29,7 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(helmet());
+app.use(compression());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -84,5 +86,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
--- routes/home/index.js
+++ routes/home/index.js
@@ -6,6 +6,12 @@ router.get('/', function(req, res, next) {
     res.render('home/index');
 });
 
+//Robots.txt
+router.get('/robots.txt', function (req, res) {
+    res.type('text/plain');
+    res.send("User-agent: *\nDisallow: /");
+});
+
 router.get('/downloadLeaflet', function(req,res,next){
   var file = __dirname + '/../../public/pdfs/SansaarLeaflet.pdf';
   console.log(file);
